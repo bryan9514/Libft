bfe9df6ab7b6140a4157efcd76838dde0667e2fa
Bryan Turcios <brturcio@1E4.42angouleme.fr>
2024-11-07 18:40:51 +0100

    07-nov-ecole

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,31 +6,34 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:27:25 by brturcio          #+#    #+#             */
-/*   Updated: 2024/11/06 17:48:24 by brturcio         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:38:33 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*ptr;
-	size_t			i;
-	size_t			total_s;
+	// size_t	i;
+	size_t	total_s;
 
-	// if (nmemb > INT_MAX || size > INT_MAX)
-	// 	return (NULL);
+
+	if (size > INT_MAX )
+		return (NULL);
 	total_s = (nmemb * size);
 	ptr = malloc(total_s);
 	if (!ptr)
 		return (NULL);
-	// ft_bzero(ptr, size);
-	i = 0;
-	while (i < total_s)
-	{
-		ptr[i] = '\0';
-		i++;
-	}
+	ft_bzero(ptr, total_s);
+
+	// i = 0;
+	// while (i < total_s)
+	// {
+	// 	ptr[i] = '\0';
+	// 	i++;
+	// }
 	return (ptr);
 }
 
