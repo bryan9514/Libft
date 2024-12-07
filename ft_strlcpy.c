42a98e969f91f115ddf755d7265ab998ebf2e6c1
Bryan Turcios <brturcio@2G6.42angouleme.fr>
2024-12-07 14:03:59 +0100

    07-dic-ecole-final

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 16:13:43 by brturcio          #+#    #+#             */
-/*   Updated: 2024/11/06 15:40:54 by brturcio         ###   ########.fr       */
+/*   Updated: 2024/11/10 15:35:57 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	src_len;
 
-	src_len = 0;
-	while (src[src_len] != '\0')
-		src_len++;
+	i = 0;
+	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
-	if (size > 0)
+	while (src[i] && i < size - 1)
 	{
-		i = 0;
-		while (src[i] != '\0' && i < size - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
+		dst[i] = src[i];
+		i++;
 	}
 	dst[i] = '\0';
 	return (src_len);
